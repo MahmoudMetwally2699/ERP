2760c75f62a153a301a031fe39619e0544cd7b3c
MahmoudMetwally2699 <mahmoud_201900756@fci.helwan.edu.eg>
2023-08-06 17:31:58 +0300

    navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Router from "./Router";
 
 // component
-import NavBar from "./UI/Utils/NavBarMA/NavBar";
+import NavBar from "./UI/Utils/NavBar/NavBar";
 import Footer from "./UI/Utils/Footer/Footer";
 export const App = () => {
   return (
